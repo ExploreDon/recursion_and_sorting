bc796cc9bdd31ab5a020ad6949ff7c74e044d806
Thapelo Nkosi <Thapelo@explore-datascience.net>
2019-03-19 12:45:54 +0200

    Updated recusrion

--- recursionsort/recursion.py
+++ recursionsort/recursion.py
@@ -22,7 +22,10 @@ def factorial(n):
     if n == 1:
         return n
     else:
-        return n*factorial(n-1)
+        lower_fact = factorial(n-1)
+        current_fact = n * lower_fact
+
+    return  current_fact
 
 
 def reverse(word):
