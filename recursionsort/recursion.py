9b6bc1cc571cce1c617f81e291618e215eccee02
Thapelo Nkosi <Thapelo@explore-datascience.net>
2019-03-19 12:49:46 +0200

    Updated recusrion

--- recursionsort/recursion.py
+++ recursionsort/recursion.py
@@ -19,13 +19,14 @@ def fibonacci(n):
 def factorial(n):
 
     '''Return n!'''
-    if n == 1:
-        return n
-    else:
-        lower_fact = factorial(n-1)
-        current_fact = n * lower_fact
+    result = 1
+    count = 2
 
-    return  current_fact
+    while count <= n:
+        result = result * count
+        count = count + 1
+
+    return result
 
 
 def reverse(word):
