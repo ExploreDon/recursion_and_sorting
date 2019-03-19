7614c346c0526054b047aea6bca981817dd69b4b
Thapelo Nkosi <Thapelo@explore-datascience.net>
2019-03-19 12:26:29 +0200

    Second commit

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='recursion_and_sorting',
+    name='recursionsort',
     version='0.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='Python package with different sorting and recursion functions',
     long_description=open('README.md').read(),
     install_requires=['numpy'],
-    url='https://github.com/<ExploreDon>/<recursion_and_sorting>',
+    url='https://github.com/<ExploreDon>/<recursionsort>',
     author='<Thapelo Nkosi>',
     author_email='<alutabnkosi@gmail.com>'
 )
